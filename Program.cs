3fd613590ef8349fb9c23ab183e23d4faf150b4d
Cracker Delicious <crackerdelicious@gmail.com>
2023-08-17 20:08:23 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FibonacciSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fibonacci Sequence Generator\n");
+
+            Console.Write("Enter the number of terms: ");
+            if (int.TryParse(Console.ReadLine(), out int terms) && terms > 0)
+            {
+                GenerateFibonacci(terms);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a positive integer.");
+            }
+        }
+
+        static void GenerateFibonacci(int terms)
+        {
+            int firstTerm = 0;
+            int secondTerm = 1;
+
+            Console.Write("Fibonacci Sequence: ");
+
+            if (terms >= 1)
+            {
+                Console.Write(firstTerm);
+            }
+
+            for (int i = 0; i < terms; i++)
+            {
+                Console.Write(", " + secondTerm);
+
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm; 
+                secondTerm = nextTerm;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
